8e1e98c1744379b49065c2f20f5b68898a14400e
Tra-NB <tran.ngocbao.tran.jb@gmail.com>
2026-01-06 02:26:05 +0700

    feat: refactor render logic with createElement and add filter/dark-mode features

--- script.js
+++ script.js
@@ -35,10 +35,6 @@ function renderExtensions(extension){
     }
 
     const htmlContent = extension.map(item => {
-
-        const statusClass = item.isActive? 'active' : 'inactive';
-        const statusText = item.isActive? 'active' : 'inactive';
-
         return`
         <article class="extension-card">
             <div class="card-header">
@@ -49,22 +45,37 @@ function renderExtensions(extension){
                     onerror="this.src='https://placehold.co/40x40?text=Icon'"
                 >
 
-                <h3 class="card-title">${item.name}</h3>
+                <div> 
+                    <h3 class="card-title">${item.name}</h3>
+                    <p class="card-desc">${item.description}</p>
+                </div>
             </div>
 
-            <p class="card-desc">${item.description}</p>
 
             <div class="card-footer">
-                <span class="status-badge ${statusClass}">${statusText}</span>
                 <button class="btn-remove" onclick="removeExtension('${item.name}')">
                     Remove
                 </button>                
+               <div class="toggle-box ${item.isActive ? 'active' : ''}" onclick="handleToggle('${item.name}')">
+                    <div class="dot"></div>
+                </div>
             </div>
         </article>
         `;
     }).join('');
 
     listContainer.innerHTML = htmlContent;
+}
+
+function handleToggle(name){
+    allExtensions = allExtensions.map(item => {
+        if(item.name === name){
+            return{...item, isActive: !item.isActive};
+        }
+        return item;
+    });
+
+    renderExtensions(allExtensions);    
 }
 
 function removeExtension(namToDelete){
@@ -80,16 +91,42 @@ loadExtensions();
 
 // DARK MODE
 const toggleButton = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
 const body = document.body;
 
-if(toggleButton){
+if(toggleButton && themeIcon){
     toggleButton.addEventListener('click',() =>{
         body.classList.toggle('dark-mode');
 
         if(body.classList.contains('dark-mode')){
-            toggleButton.textContent = "Light mode";
+           themeIcon.src = "./assets/images/icon-sun.svg";
         }else{
-            toggleButton.textContent = "Dark mode";
+          themeIcon.src = "./assets/images/icon-moon.svg";
         }
     });
+}
+
+// Lọc dữ liệu
+function filterData(status) {
+    
+    const buttons = document.querySelectorAll('.filter-group button');
+    buttons.forEach(btn => {
+        btn.classList.remove('active'); 
+        
+        if (btn.innerText.toLowerCase() === status) {
+            btn.classList.add('active');
+        }
+    });
+
+    let filteredExtensions = [];
+
+    if (status === 'all') {
+        filteredExtensions = allExtensions;
+    } else if (status === 'active') {
+        filteredExtensions = allExtensions.filter(item => item.isActive === true);
+    } else if (status === 'inactive') {
+        filteredExtensions = allExtensions.filter(item => item.isActive === false);
+    }
+
+    renderExtensions(filteredExtensions);
 }
